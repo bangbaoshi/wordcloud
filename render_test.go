3765823e11f6104c02d2b8688362b5ac0e6b04c6
bangbaoshi <541196671@qq.com>
2020-10-02 12:48:19 +0800

    add go mod

--- render_test.go
+++ render_test.go
@@ -10,7 +10,7 @@ import (
 
 func renderNow() {
 	textList := []string{"恭喜", "发财", "万事", "如意"}
-	angles := []int{0, 15, -15, 90}
+	angles := []int{0}
 	colors := []*color.RGBA{
 		&color.RGBA{0x0, 0x60, 0x30, 0xff},
 		&color.RGBA{0x60, 0x0, 0x0, 0xff},
@@ -23,8 +23,8 @@ func renderNow() {
 }
 
 func TestNewWordCloudRender(t *testing.T) {
-	startedAt := time.Now().Unix()
+	startedAt := time.Now().UnixNano()
 	renderNow()
-	endAt := time.Now().Unix()
+	endAt := time.Now().UnixNano()
 	fmt.Printf("时间消耗:%d\n", endAt-startedAt)
 }
